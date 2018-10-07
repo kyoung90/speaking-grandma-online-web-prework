a52d16b055762830fed85d1e18d371598b119d72
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-07 01:52:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(str)
   elsif (str.upcase == str)
     return "NO, NOT SINCE 1938!"
   else 
-    return "HUH?! SPEAK UPm SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end 
 # Whatever you say to grandma, she should respond with
